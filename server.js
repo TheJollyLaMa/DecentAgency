5b0d4e53313c85b8396b1ead50cba55d8c5de205
TheJollyLaMa <j@caffeinelamanna.com>
2022-02-04 16:13:22 -0500

    change to node server hostname

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const app = require("./index");
 
 if (process.env.NODE_ENV !== 'test') {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT, '127.0.0.1', () => {
         console.log(`Server is running on port: ${process.env.PORT}`);
     });
 }else{
