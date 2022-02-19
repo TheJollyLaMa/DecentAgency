019489e77bee9a5f1b68658fecf1cacf5b45e1ba
Justin LaManna <j@Justins-Mac-mini.local>
2022-02-19 08:38:36 -0500

    added landing style and mentor/wallet buttons

--- public/js/controllers/DecentAgencyController.js
+++ public/js/controllers/DecentAgencyController.js
@@ -1,7 +1,14 @@
 "use strict";
 app.controller("DecentAgencyController", ["$scope", "DecentAgencyFactory", function($scope, DecentAgencyFactory) {
   var init = function () {
-      $scope.title = {msg: 'Decent Agency HomePage', agency: "The ability to transform energy that arises from a combination of chioce and action."};
+      $scope.title = {msg: 'Decent Agency HomePage'};
+      $scope.dictionary = {
+        Decent: "Of an acceptable standard; satisfactory; abbr. for Decentral(ize)(d)",
+        Agency: "The ability to transform energy that arises from a combination of chioce and action; The ability to interact with and affect a system and/or its components; A collection of individuals with such capacity to affect change in a system.",
+        Mentor: "An experienced and trusted advisor.",
+        Employee: "A person given work and paid for it, especially at a non-executive level.",
+        Contribute: "To give in order to help achieve or provide something; help to cause or bring about; supply for publication."
+      };
       $scope.about = {msg: "A Decent Agency By Deed."};
       $scope.fetchDecentAgencyData = async () => {
         await DecentAgencyFactory.FetchDecentAgencyData().then((res)=>{
